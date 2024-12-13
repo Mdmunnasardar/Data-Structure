c283417168431f2468ca6680ed0e0c119ff586c0
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2024-12-14 02:02:53 +0600

    updating deletion

---
+++ Llinked List/Insertion/SinglyALLinsertion.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int value) : data(value), next(nullptr) {}
+};
+
+class LinkedList
+{
+private:
+    Node *head;
+
+public:
+    LinkedList() : head(nullptr) {}
+
+    void Traversal()
+    {
+        Node *ptr = head;
+
+        while (ptr != nullptr)
+        {
+            cout << ptr->data << " ";
+            ptr = ptr->next;
+        }
+        cout << endl;
+    }
+    void InsertAtHead(int data)
+    {
+
+        Node *newNode = new Node(data);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    void AfterANode(int target, int data)
+    {
+        Node *current = head;
+        Node *newNode = new Node(data);
+        newNode->next = current->next;
+        current->next = newNode;
+    }
+    void AtLastNode(int data)
+    {
+        Node *newNode = new Node(data);
+        Node *temp = head;
+        while (temp->next != nullptr)
+        {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+};
+int main()
+{
+    LinkedList ob;
+    ob.InsertAtHead(40);
+    ob.InsertAtHead(30);
+    ob.InsertAtHead(20);
+    ob.InsertAtHead(10);
+    cout << "Linked List: ";
+    ob.Traversal();
+    ob.AfterANode(30, 50);
+    cout << "After a node Insert: ";
+    ob.Traversal();
+    ob.AtLastNode(60);
+    cout << "At last Node: ";
+    ob.Traversal();
+
+   
+
+    return 0;
+}
