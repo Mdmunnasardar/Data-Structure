ef31660ab94371335c1450a45fb21772629da253
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2025-01-21 19:37:53 +0600

    all in one (insertion,deletion,seraching ) Tree

---
+++ Tree/All_In_One.cpp
@@ -0,0 +1,155 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    // Constructor
+    Node(int value) : data(value), left(nullptr), right(nullptr) {}
+};
+
+class BinarySearchTree {
+private:
+    Node* root;
+
+    Node* insertNode(Node* node, int value) {
+        if (node == nullptr) {
+            return new Node(value);
+        }
+
+        if (value < node->data) {
+            node->left = insertNode(node->left, value);
+        } else if (value > node->data) {
+            node->right = insertNode(node->right, value);
+        }
+
+        return node;
+    }
+
+    Node* findMin(Node* node) {
+        while (node->left != nullptr) {
+            node = node->left;
+        }
+        return node;
+    }
+
+    Node* deleteNode(Node* node, int value) {
+        if (node == nullptr) {
+            return nullptr;
+        }
+
+        if (value < node->data) {
+            node->left = deleteNode(node->left, value);
+        } else if (value > node->data) {
+            node->right = deleteNode(node->right, value);
+        } else {
+            if (node->left == nullptr && node->right == nullptr) {
+                delete node;
+                return nullptr;
+            }
+
+            if (node->left == nullptr) {
+                Node* temp = node->right;
+                delete node;
+                return temp;
+            }
+            if (node->right == nullptr) {
+                Node* temp = node->left;
+                delete node;
+                return temp;
+            }
+
+            Node* temp = findMin(node->right);
+            node->data = temp->data;
+            node->right = deleteNode(node->right, temp->data);
+        }
+
+        return node;
+    }
+
+    void inOrderTraversal(Node* node) {
+        if (node != nullptr) {
+            inOrderTraversal(node->left);
+            cout << node->data << " ";
+            inOrderTraversal(node->right);
+        }
+    }
+
+    bool searchNode(Node* node, int value) {
+        if (node == nullptr) {
+            return false;
+        }
+
+        if (value == node->data) {
+            return true;
+        }
+
+        if (value < node->data) {
+            return searchNode(node->left, value);
+        } else {
+            return searchNode(node->right, value);
+        }
+    }
+
+public:
+    BinarySearchTree() : root(nullptr) {}
+
+    void insert(int value) {
+        root = insertNode(root, value);
+    }
+
+    void deleteValue(int value) {
+        root = deleteNode(root, value);
+    }
+
+    void inOrder() {
+        inOrderTraversal(root);
+        cout << endl;
+    }
+
+    bool search(int value) {
+        return searchNode(root, value);
+    }
+};
+
+// Main function
+int main() {
+    BinarySearchTree bst;
+
+    // Insert nodes into the BST
+    bst.insert(50);
+    bst.insert(30);
+    bst.insert(70);
+    bst.insert(20);
+    bst.insert(40);
+    bst.insert(60);
+    bst.insert(80);
+
+    // Print the BST in in-order traversal
+    cout << "In-order Traversal before deletion: ";
+    bst.inOrder();
+
+    // Delete some nodes
+    bst.deleteValue(20);
+    bst.deleteValue(30);
+    bst.deleteValue(50);
+
+    cout << "In-order Traversal after deletions: ";
+    bst.inOrder();
+
+    // Search for a value interactively
+    int target;
+    cout << "Enter a number to search: ";
+    cin >> target;
+
+    if (bst.search(target)) {
+        cout << target << " is found in the BST." << endl;
+    } else {
+        cout << target << " is not found in the BST." << endl;
+    }
+
+    return 0;
+}
