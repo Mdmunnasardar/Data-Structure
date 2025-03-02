bb0bc48958e40c9a654fbfb9048594d535ea44a9
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2025-03-02 12:38:50 +0600

    Changing in command

---
+++ Tree/LinearSearch.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target)
+            return i; // Found, return index
+    }
+    return -1; // Not found
+}
+
+int main() {
+    int arr[] = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 23;
+
+    int result = linearSearch(arr, size, target);
+
+    if (result != -1)
+        cout << "Element found at index " << result << endl;
+    else
+        cout << "Element not found" << endl;
+
+    return 0;
+}
